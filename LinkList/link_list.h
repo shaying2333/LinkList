0a56f9566568df1786bb21cbd960d5c70c4e43e8
ruishengguo <ruishengguo@outlook.com>
2025-11-28 13:46:13 +0800

    2

--- LinkList/link_list.h
+++ LinkList/link_list.h
@@ -1,1 +1,25 @@
 #pragma once
+template<typename T>
+class Node {
+private:
+	Node<T>* nxt, last;
+	T val;
+	template<typename _Ty>
+	friend class LinkList;
+public:
+	Node() : nxt(nullptr), last(nullptr), val() {}
+	Node(const T& _val) : nxt(nullptr), last(nullptr), val(_val) {}
+	Node(const Node& other) : nxt(nullptr), last(nullptr), val(other.val) {}
+	Node& operator=(const T& _val) {
+		val = _val;
+		return *this;
+	}
+	Node& operator=(const Node& other) {
+		val = other.val;
+		return *this;
+	}
+	virtual ~Node() {
+		nxt = nullptr;
+		last = nullptr;
+	}
+};
